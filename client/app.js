622b94877c02ee95db6531d3e1588ba4eb7f02c7
Bao Pham <gbaopham@gmail.com>
2014-11-04 22:25:17 -0800

    Accounts support

--- client/app.js
+++ client/app.js
@@ -10,3 +10,7 @@ App.helpers = {
 _.each(App.helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper);
 });
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
