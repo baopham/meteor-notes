5cc39b0e29ad098588ae1d772dd3c4801166467c
Bao Pham <gbaopham@gmail.com>
2014-11-07 02:12:07 -0800

    Fix wrong query

--- client/views/notes/notes_index/notes_index.js
+++ client/views/notes/notes_index/notes_index.js
@@ -24,7 +24,7 @@ Template.NotesIndex.helpers({
     if (Session.get('selected-note')) {
       return Notes.findOne(Session.get('selected-note'));
     } else {
-      var note = Notes.findOne({}, { createdAt: -1 });
+      var note = Notes.findOne({}, { sort: { createdAt: -1 } });
       Session.set('selected-note', note._id);
       return note;
     }
