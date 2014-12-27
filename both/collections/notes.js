2ac245af5b948c4ef623e856d59ddaab0cf7b5f5
Bao Pham <gbaopham@gmail.com>
2014-12-26 21:50:29 -0800

    Search entire db instead of just published ones

--- both/collections/notes.js
+++ both/collections/notes.js
@@ -1,7 +1,17 @@
 Notes = new Mongo.Collection('notes');
+
 Notes.initEasySearch(['content', 'title'], {
-  limit: 20
+  limit: 20,
+  use: 'mongo-db',
+  query: function () {
+    var selector = {};
+    selector['$or'] = [];
+    selector['$or'].push({ public: true });
+    selector['$or'].push({ owner: this.publishScope.userId });
+    return selector;
+  }
 });
+
 /*
  * Add query methods like this:
  *  Notes.findPublic = function () {
