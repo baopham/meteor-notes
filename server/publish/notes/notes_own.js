0498c74f71fbb610ec5fef4029e4af1577dc33c5
Bao Pham <gbaopham@gmail.com>
2014-12-26 20:24:48 -0800

    Pass in limit for publication notes.own

--- server/publish/notes/notes_own.js
+++ server/publish/notes/notes_own.js
@@ -2,9 +2,9 @@
 /* NotesOwn Publish Functions
 /*****************************************************************************/
 
-Meteor.publish('notes.own', function () {
+Meteor.publish('notes.own', function (limit) {
   if (this.userId) {
-    return Notes.find({ owner: this.userId });
+    return Notes.find({ owner: this.userId }, { limit: limit, sort: { createdAt: -1 } });
   }
   return [];
 });
